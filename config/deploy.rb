515572f3e8982ed0d13414a98dae5df34c0e3973
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-09 08:04:23 -0300

    Modificando a sintaxe das hash's para ruby 1.8 no arquivo deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "bundler/capistrano"
 
-server "50.116.34.120", :web, :app, :db, primary: true
+server "50.116.34.120", :web, :app, :db, :primary => true
 
 set :application, "cidadedemocratica"
 set :user, "cidadedemocratica"
@@ -20,11 +20,11 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
 namespace :deploy do
   task :start do; end
   task :stop do; end
-  task :restart, roles: :app, except: {no_release: true} do
+  task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{deploy_to}/current/tmp/restart.txt"
   end
 
-  task :setup_config, roles: :app do
+  task :setup_config, :roles => :app do
     sudo "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
@@ -32,13 +32,13 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
+  task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
+  task :check_revision, :roles => :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
