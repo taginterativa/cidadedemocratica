f6ae39c15a197f2b52b655a4e8b3c215c24db285
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-13 15:22:12 -0300

    Adicionando métodos faltantes no model tag

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -1,5 +1,22 @@
 class Tag < ActiveRecord::Base
 
+  # LIKE is used for cross-database case-insensitivity
+  def self.find_or_create_with_like_by_name(name)
+    find(:first, :conditions => ["name LIKE ?", name]) || create(:name => name)
+  end
+  
+  def ==(object)
+    super || (object.is_a?(Tag) && name == object.name)
+  end
+  
+  def to_s
+    name
+  end
+  
+  def count
+    read_attribute(:count).to_i
+  end
+
   def slug
     self.name.remove_acentos
   end
