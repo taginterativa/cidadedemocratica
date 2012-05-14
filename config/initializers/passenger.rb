7e2b3df412188c418ee3abf610218bd099060081
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-14 02:58:10 -0300

    Fix no nome do evento disparado pelo passenger

--- config/initializers/passenger.rb
+++ config/initializers/passenger.rb
@@ -1,5 +1,5 @@
 if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_proccess) do |forked|
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
       Rails.cache.instance_variable_get(:@data).reset if Rails.cache.class == ActiveSupport::Cache::MemCacheStore
     end
